bc7bb793acd87c83ed22cdbb53a614d7f7f2692d
Jortjorisleroij <111754184+Jortjorisleroij@users.noreply.github.com>
2024-03-15 11:30:16 +0100

    PLC_communication.py

--- PLC_communication.py
+++ PLC_communication.py
@@ -1,37 +1,47 @@
 import snap7
+import numpy as np
 
 
 def get_start_offset(valve, num_valves):
-    if valve < 1 or valve > num_valves:
+    if valve < 0 or valve > num_valves:
         print("Incorrect valve provided.")
         return None
 
-    if valve <= num_valves / 3:
+    if valve + 1<= num_valves / 3:
         return 0
-    elif valve <= (num_valves / 3) * 2:
+    elif valve + 1<= (num_valves / 3) * 2:
         return 1
     else:
         return 2
 
 
 def get_bit_offset(valve, num_valves):
-    if valve < 1 or valve > num_valves:
+    if valve < 0 or valve > num_valves:
         print("Incorrect valve provided.")
         return None
+    
+    bt_off = (valve) % (num_valves // 3)
+    return bt_off + 1 if bt_off % 2 == 0 else bt_off - 1  
 
-    return (valve - 1) % (num_valves // 3) + 1
 
-
-def write_bool(valve, value, num_valves):
+def write_bool(plc, valve, value, num_valves):
     start_offset = get_start_offset(valve, num_valves)
     bit_offset = get_bit_offset(valve, num_valves)
 
-    reading = plc.db_read(DB_NUMBER, start_offset, 1)  # (db number, start offset, read 1 byte)
+    reading = plc.db_read(1, start_offset, 1)  # (db number, start offset, read 1 byte)
     snap7.util.set_bool(reading, 0, bit_offset, value)  # (value 1 = True; 0 = False)
     # (bytearray_: bytearray, byte_index: int, bool_index: int, value: bool)
-    plc.db_write(DB_NUMBER, start_offset, reading)  # Write the byte array. The boolean should change
+    plc.db_write(1, start_offset, reading)  # Write the byte array. The boolean should change
 
 
-def write_values(valves, value, num_valves):
+def write_values(plc, valves, value, num_valves):
     for num in valves:
-        write_bool(num, value, num_valves)
+        write_bool(plc, num, value, num_valves)
+
+
+if __name__ == '__main__':
+    plc = snap7.client.Client()
+    plc.connect('192.168.0.1', 0, 1)  # IP address, rack, slot (from HW settings)
+    write_values(plc, np.array([9, 10, 13, 14]), 0, 24)
+    
+
