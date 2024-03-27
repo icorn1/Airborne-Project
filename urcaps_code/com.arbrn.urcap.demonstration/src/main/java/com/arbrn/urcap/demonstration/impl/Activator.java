8dd5b5588bbc6387fe895298284d7fb716a95259
icorn1 <ixent.cornella@estudiantat.upc.edu>
2024-03-27 11:19:27 +0100

    Add URCap for GUI (Demonstration)

---
+++ urcaps_code/com.arbrn.urcap.demonstration/src/main/java/com/arbrn/urcap/demonstration/impl/Activator.java
@@ -0,0 +1,22 @@
+package com.arbrn.urcap.demonstration.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import com.ur.urcap.api.contribution.installation.swing.SwingInstallationNodeService;
+
+/**
+ * Hello world activator for the OSGi bundle URCAPS contribution
+ *
+ */
+public class Activator implements BundleActivator {
+	@Override
+	public void start(BundleContext bundleContext) throws Exception {
+		bundleContext.registerService(SwingInstallationNodeService.class, new TestInstallationNodeService(), null);
+	}
+
+	@Override
+	public void stop(BundleContext bundleContext) throws Exception {
+	}
+}
+
---
+++ urcaps_code/com.arbrn.urcap.demonstration/src/main/java/com/arbrn/urcap/demonstration/impl/TestInstallationNodeService.java
@@ -0,0 +1,34 @@
+package com.arbrn.urcap.demonstration.impl;
+
+import java.util.Locale;
+
+import com.ur.urcap.api.contribution.ViewAPIProvider;
+import com.ur.urcap.api.contribution.installation.ContributionConfiguration;
+import com.ur.urcap.api.contribution.installation.CreationContext;
+import com.ur.urcap.api.contribution.installation.InstallationAPIProvider;
+import com.ur.urcap.api.contribution.installation.swing.SwingInstallationNodeService;
+import com.ur.urcap.api.domain.data.DataModel;
+
+public class TestInstallationNodeService implements SwingInstallationNodeService<TestInstallationNodeContribution, TestInstallationNodeView>{
+
+	@Override
+	public void configureContribution(ContributionConfiguration configuration) {
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return "Laminate Demonstration";
+	}
+
+	@Override
+	public TestInstallationNodeView createView(ViewAPIProvider apiProvider) {
+		return new TestInstallationNodeView();
+	}
+
+	@Override
+	public TestInstallationNodeContribution createInstallationNode(InstallationAPIProvider apiProvider,
+			TestInstallationNodeView view, DataModel model, CreationContext context) {
+		return new TestInstallationNodeContribution(view);
+	}
+
+}
