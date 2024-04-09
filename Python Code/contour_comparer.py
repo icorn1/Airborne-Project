765db360691691c9efc6237d086e35da33abf3c7
Drellzie <150245828+Drellzie@users.noreply.github.com>
2024-04-09 14:18:11 +0200

    Update contour_comparer.py
    Added documentation

--- Python Code/contour_comparer.py
+++ Python Code/contour_comparer.py
@@ -1,30 +1,49 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import cv2
 
-ACCURACY = 15  # Pixels
-SCALING_FACTOR = 1.46  # The PNG generated from the DXF has a scale X times bigger than the images from the camera
+ACCURACY = 11  # Pixels
+SCALING_FACTOR = 1.475  # The PNG generated from the DXF has a scale X times bigger than the images from the camera
 ANGLE_OPTIMIZATION_ITERATIONS = 200
 
 
 def plot(img, cnts, fail_point_inds_per_cnt, trans_model_contour_points):
-    model_color = "C1"
-    object_color = "C0"
-    fail_color = "r."
-    plt.imshow(img, cmap="gray")
+    """
+    Plot contours, fail points, and model contours on the image.
+
+    Parameters:
+    - img (numpy.ndarray): Input image.
+    - cnts (list): List of contours.
+    - fail_point_inds_per_cnt (list): List of lists containing indices of fail points for each contour.
+    - trans_model_contour_points (list): List of contours representing the transformed model.
+
+    Explanation:
+    This function plots contours, fail points, and model contours on the input image.
+    Contours are drawn in blue, fail points in red, and model contours in blue.
+    """
+    plot_img = img.copy()
+    model_color = (255, 0, 0)  # Blue (BGR format)
+    fail_color = (0, 0, 255)  # Red
+
+    # Draw contours and fail points
     for i, cnt in enumerate(cnts):
-        plt.plot(cnt.reshape(-1, 2)[:, 0], cnt.reshape(-1, 2)[:, 1], color=object_color, label='Part')
-        if len(fail_point_inds_per_cnt[0]) > 0:
-            fail_points = cnt.reshape(-1, 2)[fail_point_inds_per_cnt[i], :]
-            plt.plot(fail_points[:, 0], fail_points[:, 1], fail_color, markersize=5, label='Fail points')
+        # Assuming fail_point_inds_per_cnt contains indices of fail points for each contour
+        fail_points = cnt[fail_point_inds_per_cnt[i]] if len(fail_point_inds_per_cnt[i]) > 0 else []
+        for point in fail_points:
+            cv2.circle(plot_img, tuple(point[0]), 5, fail_color, -1)  # Draw fail points
 
-    for to_object in [trans_model_contour_points]:
-        to_object = np.append(to_object, [to_object[0]], axis=0)
-        plt.plot(to_object.reshape(-1, 2)[:, 0], to_object.reshape(-1, 2)[:, 1], color=model_color, label='Model')
+    # Convert each contour in trans_model_contour_points to the correct format
+    for i in range(len(trans_model_contour_points)):
+        trans_model_contour_points[i] = np.array(trans_model_contour_points[i], dtype=np.int32)
 
-    plt.legend()
-    plt.show()
+    # Draw contours on the image
+    cv2.drawContours(plot_img, trans_model_contour_points, -1, model_color, 2)
+
+    # Display the image
+    pic = cv2.resize(plot_img, (1080, 858))
+    cv2.imshow("Image", pic)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def load_contour(filename):
@@ -33,6 +52,9 @@ def load_contour(filename):
 
     :param filename: The name of the text file containing the contour data.
     :return: Loaded contour represented as a NumPy array with shape (-1, 1, 2).
+
+    The text file should contain the contour data in a format compatible with NumPy's loadtxt function,
+    such as comma-separated or space-separated values.
     """
     # Load the contour from the text file
     loaded_contour_reshaped = np.loadtxt(filename)
@@ -46,8 +68,7 @@ def load_contour(filename):
 
 
 def get_orientation_pca(pts):
-    """Function calculates the orientation of the object based on the
-    angle of its first principal axes.
+    """Function calculates the orientation of the object based on the angle of its first principal axes.
 
     Args:
         pts (n x 2 numpy array): xy points
@@ -92,112 +113,172 @@ def get_trans_matrix_and_transform(theta, center_point, center_shift, contour):
 
 
 def find_transform(model_contours, cnts, max_dev):
+    """
+    Find the transformation between model contours and target contours.
+
+    Args:
+        model_contours (list): List of model contours.
+        cnts (list): List of target contours.
+        max_dev (float): Maximum allowed deviation for matching points.
+
+    Returns:
+        tuple: Transformed target contours, optimal rotation angle, minimum sum of distances.
+
+    Explanation:
+    This function finds the transformation between the model contours and target contours.
+    It calculates the orientation and center of the model and target contours, computes the angle
+    difference and center shift between them, and applies a transformation to the model contours.
+    It then iteratively optimizes the rotation angle to minimize the sum of distances between
+    matched points. Finally, it returns the transformed target contours, optimal rotation angle,
+    and minimum sum of distances.
+    """
+    # Calculate orientation and center of model and target contours
     theta_model, center_model = get_orientation_pca(model_contours[0])
     theta_part, center_part = get_orientation_pca(cnts[0])
+
+    # Calculate angle difference and center shift
     theta = theta_part - theta_model
     center_shift = [center_part[0] - center_model[0], center_part[1] - center_model[1]]
 
-    cnt_trans, trans_matrix = get_trans_matrix_and_transform(theta, center_model, center_shift,
-                                                             model_contours[0])
+    # Get initial transformed contours and transformation matrix
+    cnt_trans, trans_matrix = get_trans_matrix_and_transform(theta, center_model, center_shift, model_contours[0])
 
-    distances, _ = NearestNeighbors(n_neighbors=1).fit(
-        cnt_trans.reshape(-1, 2)).kneighbors(cnts[0].reshape(-1, 2))
+    # Compute nearest neighbor distances
+    distances, _ = NearestNeighbors(n_neighbors=1).fit(cnt_trans.reshape(-1, 2)).kneighbors(cnts[0].reshape(-1, 2))
 
+    # Initialize minimum sum of distances and best angle
     min_sum = np.sum(distances)
     best_angle = theta
+
+    # If max deviation is exceeded, perform angle optimization
     if max(distances) > max_dev:
         angles = np.linspace(theta - np.pi, theta + np.pi, num=ANGLE_OPTIMIZATION_ITERATIONS)
-        
-        for angle in angles:
-            cnt_trans, trans_matrix = get_trans_matrix_and_transform(angle, center_model, center_shift,
-                                                                     model_contours[0])
-            distances, _ = NearestNeighbors(n_neighbors=1).fit(
-                cnt_trans.reshape(-1, 2)).kneighbors(cnts[0].reshape(-1, 2))
 
+        # Iterate over angles for optimization
+        for angle in angles:
+            # Get transformed contours and transformation matrix for the current angle
+            cnt_trans, trans_matrix = get_trans_matrix_and_transform(angle, center_model, center_shift, model_contours[0])
+
+            # Compute nearest neighbor distances
+            distances, _ = NearestNeighbors(n_neighbors=1).fit(cnt_trans.reshape(-1, 2)).kneighbors(cnts[0].reshape(-1, 2))
+
+            # Update minimum sum of distances and best angle if improvement is found
             if np.sum(distances) < min_sum:
                 min_sum = np.sum(distances)
                 best_angle = angle
-                
+
+            # If max deviation condition is met, stop optimization
             if max(distances) < max_dev:
                 break
-    cnt_trans, trans_matrix = get_trans_matrix_and_transform(best_angle, center_model, center_shift,
-                                                             model_contours[0])
+
+    # Get final transformed contours and return results
+    cnt_trans, trans_matrix = get_trans_matrix_and_transform(best_angle, center_model, center_shift, model_contours[0])
     return cnt_trans, best_angle, min_sum
 
 
 def match_contour_to_model(cnts, model_contours, max_dev, img_ppmm, img, show_plot=False):
-    """Checking if the contours got from the image match with the
-    defined model contours.
+    """
+    Check if the contours extracted from the image match the defined model contours.
 
     Args:
-        cnts (list of numpy arrays): Contours list as returned by OpenCV
-        model_contours (list of numpy arrays): Contours list as returned by OpenCV
-        max_dev (float): Maximum deviation from the model (in pixels)
+        cnts (list of numpy arrays): Contours obtained from the image.
+        model_contours (list of numpy arrays): Defined model contours.
+        max_dev (float): Maximum deviation from the model (in pixels).
         img_ppmm (float): Pixel per millimeter ratio for the image.
         img (numpy array): Image to overlay the results on.
-        show_plot (bool): Whether to plot the results or not.
+        show_plot (bool): Whether to plot the results.
 
     Raises:
-        ValueError: If the parameter figure_mode is not "show" or "return"
+        ValueError: If the parameter figure_mode is not "show" or "return".
 
     Returns:
-        bool, numpy array: Did the part pass, the result image (None if figure_mode=="show")
+        tuple: Rotation angle, minimum sum of distances, pass/fail result, fail reason.
     """
 
     final_result = True
     fail_reason = " "
 
+    # Check if the number of contours match
     if len(cnts) != len(model_contours):
         final_result = False
-        fail_reason = "The number of holes do not match"
+        fail_reason = "The number of contours do not match"
 
-    scale = img_ppmm 
-    model_contours = model_contours
-    model_contours[0] = model_contours[0] * scale
+    # Scale model contours based on pixel per millimeter ratio
+    model_contours_scaled = [model_contours[0] * img_ppmm]
 
-    cnt_trans, best_angle, min_sum = find_transform(model_contours, cnts, max_dev)
+    # Find the transformation between model and target contours
+    cnt_trans, best_angle, min_sum = find_transform(model_contours_scaled, cnts, max_dev)
 
-    cnt_trans = cnt_trans.reshape(-1, 2)
+    # Initialize dictionary to store contour indices and corresponding model indices
     cnt_inds_and_mdl_inds = {0: 0}
-    trans_model_contour_points = [cnt_trans]
 
-    # Going their through one by one and checking the distances.
+    # Initialize list to store transformed model contours
+    trans_model_contour_points = [cnt_trans.reshape(-1, 2)]
+
+    # Initialize list to store indices of fail points for each contour
     fail_point_inds_per_cnt = [[]]
+
+    # Check distances between contours and transformed model
     for cnt_ind in cnt_inds_and_mdl_inds:
         mdl_ind = cnt_inds_and_mdl_inds[cnt_ind]
         distances, _ = NearestNeighbors(n_neighbors=1).fit(
             trans_model_contour_points[mdl_ind]).kneighbors(cnts[cnt_ind].reshape(-1, 2))
         is_over = distances.reshape(1, -1)[0] > max_dev
         fail_point_inds_per_cnt[cnt_ind], = np.where(is_over)
-        if len(fail_point_inds_per_cnt[0]) > 0:
+        if len(fail_point_inds_per_cnt[cnt_ind]) > 0:
             if final_result:
                 final_result = False
-                fail_reason = "Too big a deviation"
+                fail_reason = "Exceeded maximum deviation"
 
+    # Plot results if show_plot is True
     if show_plot:
-        plot(img, cnts, fail_point_inds_per_cnt, trans_model_contour_points)
+        plot(img, cnts, fail_point_inds_per_cnt, [cnt_trans])
 
     return best_angle, min_sum, final_result, fail_reason
 
 
 def find_best_match(contours, model_contour, img, show_plot=False):
-    best_score = float('inf')
-    best_index = 0
-    angle = 0
-    ret = False
+    """
+    Find the best-matching contour from a list of contours with respect to a given model contour.
 
+    Args:
+        contours (list of numpy arrays): List of contours to search through.
+        model_contour (numpy array): Model contour to match against.
+        img (numpy array): Image to overlay the results on.
+        show_plot (bool): Whether to plot the results.
+
+    Returns:
+        tuple: Index of the best-matching contour, rotation angle, success flag.
+
+    Explanation:
+    This function iterates through a list of contours and matches each contour to the given model contour
+    using the match_contour_to_model function. It returns the index of the best-matching contour, the rotation angle,
+    and a success flag indicating whether a match was found.
+    """
+    best_score = float('inf')  # Initialize best score to positive infinity
+    best_index = 0  # Initialize index of best-matching contour
+    angle = 0  # Initialize rotation angle
+    ret = False  # Initialize success flag
+
+    # Return default values if contours list is empty
     if len(contours) == 0:
         return best_index, angle, ret
 
+    # Iterate through each contour in the list
     for index, contour in enumerate(contours):
-        angle, score, ret, reason = match_contour_to_model([contour], [model_contour], ACCURACY, SCALING_FACTOR, img,
-                                                           show_plot=show_plot)
+        # Match current contour to the model contour
+        angle, score, ret, _ = match_contour_to_model([contour], [model_contour], ACCURACY, SCALING_FACTOR, img,
+                                                      show_plot=show_plot)
+
+        # If match is found, update best_index and exit loop
         if ret:
-            angle = angle
             best_index = index
             break
+
+        # Update best_score and best_index if current score is lower
         if score < best_score:
             best_score = score
-            angle = angle
             best_index = index
+
+    # Return index of best-matching contour, rotation angle, and success flag
     return best_index, angle, ret
