724d41f70ab4304c145c7bf293efcf4564da9ce2
Drellzie <150245828+Drellzie@users.noreply.github.com>
2024-04-09 13:25:15 +0200

    Update PLC_communication.py
    Added additional variables

--- Python Code/PLC_communication.py
+++ Python Code/PLC_communication.py
@@ -1,9 +1,11 @@
 import snap7
-import numpy as np
 
 # Variables
 NUMBER_OF_VALVES = 24
-DATABASE_VALUES = 1
+PLC_IP = '192.168.0.1'
+DATABASE_RACK = 0
+DATABASE_SLOT = 1
+PLC_PORT = 102
 
 
 def get_start_offset(valve, num_valves):
@@ -24,7 +26,7 @@ def get_bit_offset(valve, num_valves):
         print("Incorrect valve provided.")
         return None
 
-    bt_off = (valve) % (num_valves // 3)
+    bt_off = valve % (num_valves // 3)
     return bt_off + 1 if bt_off % 2 == 0 else bt_off - 1
 
 
@@ -32,10 +34,10 @@ def write_bool(plc, valve, value):
     start_offset = get_start_offset(valve, NUMBER_OF_VALVES)
     bit_offset = get_bit_offset(valve, NUMBER_OF_VALVES)
 
-    reading = plc.db_read(DATABASE_VALUES, start_offset, 1)  # (db number, start offset, read 1 byte)
+    reading = plc.db_read(DATABASE_SLOT, start_offset, 1)  # (db number, start offset, read 1 byte)
     snap7.util.set_bool(reading, 0, bit_offset, value)  # (value 1 = True; 0 = False)
     # (bytearray_: bytearray, byte_index: int, bool_index: int, value: bool)
-    plc.db_write(DATABASE_VALUES, start_offset, reading)  # Write the byte array. The boolean should change
+    plc.db_write(DATABASE_SLOT, start_offset, reading)  # Write the byte array. The boolean should change
 
 
 def write_values(plc, valves, value):
@@ -45,5 +47,6 @@ def write_values(plc, valves, value):
 
 if __name__ == '__main__':
     plc = snap7.client.Client()
-    plc.connect('192.168.0.1', 0, 1)  # IP address, rack, slot (from HW settings)
-    write_values(plc, np.array([9, 10, 13, 14]), 0)
+    plc.connect(PLC_IP, DATABASE_RACK, DATABASE_SLOT, PLC_PORT)
+    all_valves = list(range(0, NUMBER_OF_VALVES))
+    write_values(plc, all_valves, 0)
